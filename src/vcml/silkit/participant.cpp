2f8c3c9118236dec14dacc2bb205c9048a55458a
Simon Winther <simon@mwa.re>
2024-05-28 08:27:07 +0000

    style: fix waiting loop

--- src/vcml/silkit/participant.cpp
+++ src/vcml/silkit/participant.cpp
@@ -108,12 +108,15 @@ participant::participant(const sc_module_name& nm):
 void participant::end_of_elaboration() {
     std::unique_lock lock(m_mtx);
     m_cond_start.wait(lock, [this] { return m_start; });
-    while (m_lifecycle->State() != ParticipantState::Running);
+    while (m_lifecycle->State() != ParticipantState::Running) {
+        // nothing to do
+    }
 }
 
 participant::~participant() {
     auto state = m_lifecycle->State();
-    if (state == ParticipantState::Running || state == ParticipantState::Paused)
+    if (state == ParticipantState::Running ||
+        state == ParticipantState::Paused)
         m_lifecycle->Stop("User requested");
     else if (state != ParticipantState::Shutdown) {
         m_lifecycle->ReportError(
